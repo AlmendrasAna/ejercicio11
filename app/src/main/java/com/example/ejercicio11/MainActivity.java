3d0baf67e07ec9a4c91ce18aaed2f92c7e5bef10
Ana <almendras451@gmail.com>
2023-06-28 21:27:21 -0400

    toast lissto

--- app/src/main/java/com/example/ejercicio11/MainActivity.java
+++ app/src/main/java/com/example/ejercicio11/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.ejercicio11.databinding.ActivityMainBinding;
 
@@ -19,9 +20,51 @@ public class MainActivity extends AppCompatActivity {
         binding.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                toastShow("turn off fight mode");
+            }
+        });
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toastShow("Picture saved gallery");
             }
         });
 
+        binding.button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toastShow("Level Completed");;
+            }
+        });
+        binding.button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toastShow("Updating profile");
+            }
+
+        });
+        binding.button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toastShow("unloked level 5");
+            }
+        });
+
+        binding.button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toastShow("music paused");
+            }
+        });
     }
+
+
+    private void toastShow(String msn) {
+
+        Toast.makeText(getBaseContext(), msn, Toast.LENGTH_LONG).show();
+    }
+
+
 }
+
+
